df91642fd22df96d5356efcd6c4bc2b523f1afc3
Tekazaya Eliel <37189860+tkz1937@users.noreply.github.com>
2018-08-14 13:18:38 +0200

    Add files via upload

---
+++ info.php
@@ -0,0 +1,35 @@
+<?php
+
+/////////////////////////////////////////////////////////////////////////////
+// General information
+/////////////////////////////////////////////////////////////////////////////
+
+$app['basename'] = 'shell_right';
+$app['version'] = '2.10.1';
+$app['release'] = 'itot';
+$app['vendor'] = 'itot'; // e.g. Acme Co
+$app['packager'] = 'Packager'; // e.g. Gordie Howe
+$app['license'] = 'MyLicense'; // e.g. 'GPLv3';
+$app['license_core'] = 'MyLicense'; // e.g. 'LGPLv3';
+$app['description'] = lang('shell_right_app_description');
+$app['powered_by'] = array(
+	    'vendor' => array(
+        'name' => 'name of application',
+        'url' => '',
+    ),
+		'packages' =>  array(
+			'spackages name' => array(
+				'name' => 'name of application',
+				'version' => '2.10.1',
+				'url' => '',
+	), 
+	),
+);
+
+/////////////////////////////////////////////////////////////////////////////
+// App name and categories
+/////////////////////////////////////////////////////////////////////////////
+
+$app['name'] = lang('shell_right_app_name');
+$app['category'] = lang('base_category_system');
+$app['subcategory'] = 'Developer'; // e.g. lang('base_subcategory_settings');
